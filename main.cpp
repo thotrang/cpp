5fac88f329a97b444704a21de8ef87671cb00562
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-06-08 19:17:38 +0700

    commit 8

--- main.cpp
+++ main.cpp
@@ -2,206 +2,199 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void xuat(double a[], int n)
+void xuat(float a[], int n)
 {
-    for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
+   for (int i = 0; i < n; i++)
+      cout << a[i] << " ";
 }
 class Obj
 {
-    double x;
-    double y;
-    double z = 0;
+   float x;
+   float y;
+   float z = 0;
 
 public:
-    Obj() {}
+   Obj() {}
 
-    void setX(double x)
-    {
-        this->x = x;
-    }
-    void setY(double y)
-    {
-        this->y = y;
-    }
-    void setZ(double z)
-    {
-        this->z = z;
-    }
-    double getX()
-    {
-        return this->x;
-    }
-    double getY()
-    {
-        return this->y;
-    }
-    double getZ()
-    {
-        return this->z;
-    }
+   void setX(float x)
+   {
+      this->x = x;
+   }
+   void setY(float y)
+   {
+      this->y = y;
+   }
+   void setZ(float z)
+   {
+      this->z = z;
+   }
+   float getX()
+   {
+      return this->x;
+   }
+   float getY()
+   {
+      return this->y;
+   }
+   float getZ()
+   {
+      return this->z;
+   }
 };
-double timMin(double a[], int n)
+float timMin(float a[], int n)
 {
-    double min = a[0];
+   float min = a[0];
 
-    for (int i = 1; i < n; i++)
-    {
-        if (a[i] < min)
-        {
-            min = a[i];
-        }
-    }
+   for (int i = 1; i < n; i++)
+   {
+      if (a[i] < min)
+      {
+         min = a[i];
+      }
+   }
 
-    return min;
+   return min;
 }
 
-double timMax(double a[], int n)
+float timMax(float a[], int n)
 {
-    double max = a[0];
+   float max = a[0];
 
-    for (int i = 1; i < n; i++)
-    {
-        if (a[i] > max)
-        {
-            max = a[i];
-        }
-    }
+   for (int i = 1; i < n; i++)
+   {
+      if (a[i] > max)
+      {
+         max = a[i];
+      }
+   }
 
-    return max;
+   return max;
 }
 
-// double KhoangCachCacDiem(Obj point[5000], Obj newPoint[200], double KhoangCach[200][5000], int iX)
-// {
-//     for (int i = 0; i < iX; i++)
-//     {
-//         for (int j = 0; j < 4457; j++)
-//         {
-//             KhoangCach[i][j] = sqrt((point[j].getX() - newPoint[i].getX()) * (point[j].getX() - newPoint[i].getX()) + (point[j].getY() - newPoint[i].getY()) * (point[j].getY() - newPoint[i].getY()));
-//         }
-//     }
-// }
-// void swap(double &a, double &b)
-// {
-//     double temp = a;
-//     a = b;
-//     b = temp;
-// }
-// double _4DiemGanNhat(Obj point[5000], double KhoangCach[200][5000], int iX, Obj _4Diem [200][4])
-// {
-//     for (int i = 0; i < iX; i++)
-//     {
-//         for (int j = 0; j < 4456; j++)
-//         {
-//             for (int t = j + 1; t < 4457; t++)
-//             {
-//                 if (KhoangCach[i][j] > KhoangCach[i][t])
-//                 {
-//                     swap(KhoangCach[i][j], KhoangCach[i][t]);
-//                     swap(point[j], point[t]);
-//                 }
-//             }
-//         }
-//         _4Diem [i][0] = point[4456];
-//         _4Diem [i][1] = point[4455];
-//         _4Diem [i][2] = point[4454];
-//         _4Diem [i][3] = point[4453];
-
-//         double m1 = KhoangCach[i][4456], m2 = KhoangCach[i][4455], m3 = KhoangCach[i][4454], m4 = KhoangCach[i][4453];
-//         for (int j = 4456; j >= 0; j--)
-//         {
-//             if (KhoangCach[i][j] <= m1 && point[j].getZ() != _4Diem [i][0]){
-//                 _4Diem [i][0] = point[j];
-//                 m1=KhoangCach[i][j];
-//             }
-                
-//             else if (KhoangCach[i][j] <= m2 && point[j].getZ() != _4Diem [i][1]){
-//                 _4Diem [i][0] = point[j];
-//                 m2=KhoangCach[i][j];
-//             }
-                
-//             else if (KhoangCach[i][j] <= m3 && point[j].getZ() != _4Diem [i][2]){
-//                 _4Diem [i][0] = point[j];
-//                 m3=KhoangCach[i][j];
-//             }
-                
-//             else if (KhoangCach[i][j] <= m4 && point[j].getZ() != _4Diem [i][3]){
-//                 _4Diem [i][0] = point[j];
-//                 m4=KhoangCach[i][j];
-//             }
-                
-//         }
-//     }
-// }
+void swap(float &a, float &b)
+{
+   float temp = a;
+   a = b;
+   b = temp;
+}
 
 int main()
 {
-    int n = 4457;
-    double x, y, z;
-    Obj point[4500];
-    ofstream outfile;
-    freopen("slthu.txt", "r", stdin);
+   int n = 4457;
+   float x, y, z;
+   Obj point[4500];
+   ofstream outfile;
+   freopen("slthu.txt", "r", stdin);
 
-    for (int i = 0; i < 4457; i++)
+   for (int i = 0; i < 4457; i++)
+   {
+      cin >> x >> y >> z;
+
+      point[i].setX(x);
+      point[i].setY(y);
+      point[i].setZ(z);
+   }
+   float arrx[4500] = {};
+   float arry[4500] = {};
+   float arrz[4500] = {};
+
+   freopen("slthu.txt", "r", stdin);
+   for (int i = 0; i < 4457; i++)
+      cin >> arrx[i] >> arry[i] >> arrz[i];
+
+   float minx = timMin(arrx, n), miny = timMin(arry, n);
+   float maxx = timMax(arrx, n), maxy = timMax(arry, n);
+
+   float newX[4500] = {};
+   float newY[4500] = {};
+   int iX = 0;
+   while (minx <= maxx)
+   {
+      newX[iX] = minx;
+      minx += 0.00005;
+      iX++;
+   }
+   int iY = 0;
+   while (miny <= maxy)
+   {
+      newY[iY] = miny;
+      miny += 0.00007;
+      iY++;
+   }
+
+   Obj newPoint[150];
+   int i = 0;
+   while (i < iX)
+   {
+      newPoint[i].setX(newX[i]);
+      newPoint[i].setY(newY[i]);
+      i++;
+   }
+   Obj _4Diem[200][4];
+
+   float KhoangCach[4500] = {};
+   for (int i4 = 0; i4 < iX; i4++)
+   {
+      for (int kc = 0; kc < 4457; kc++) // tính khoảng cách newpoint với point
+      {
+         KhoangCach[kc] = sqrt((point[kc].getX() - newPoint[i4].getX()) * (point[kc].getX() - newPoint[i4].getX()) + (point[kc].getY() - newPoint[i4].getY()) * (point[kc].getY() - newPoint[i4].getY()));
+      }
+
+      for (int j4 = 0; j4 < 4456; j4++) // sắp xếp lại vị trí khoảng cách và point từ bé đến lớn
+      {
+         for (int t4 = j4 + 1; t4 < 4457; t4++)
+         {
+            if (KhoangCach[j4] > KhoangCach[t4])
+            {
+               swap(KhoangCach[j4], KhoangCach[t4]);
+               swap(point[j4], point[t4]);
+            }
+         }
+      }
+
+      _4Diem[i4][0] = point[4456];     //tìm 4 điểm của point gần với newpoint với z ko trùng nhau
+      _4Diem[i4][1] = point[4455];
+      _4Diem[i4][2] = point[4454];
+      _4Diem[i4][3] = point[4453];
+
+
+      float m1 = KhoangCach[4456], m2 = KhoangCach[4455], m3 = KhoangCach[4454], m4 = KhoangCach[4453];
+
+      for (int j5 = 4456; j5 >= 0; j5--)
+      {
+         if (KhoangCach[j5] <= m1 && point[j5].getZ() != _4Diem[i4][0].getZ())
+         {
+            _4Diem[i4][0] = point[j5];
+            m1 = KhoangCach[j5];
+         }
+
+         else if (KhoangCach[j5] <= m2 && point[j5].getZ() != _4Diem[i4][1].getZ())
+         {
+            _4Diem[i4][0] = point[j5];
+            m2 = KhoangCach[j5];
+         }
+
+         else if (KhoangCach[j5] <= m3 && point[j5].getZ() != _4Diem[i4][2].getZ())
+         {
+            _4Diem[i4][0] = point[j5];
+            m3 = KhoangCach[j5];
+         }
+
+         else if (KhoangCach[j5] <= m4 && point[j5].getZ() != _4Diem[i4][3].getZ())
+         {
+            _4Diem[i4][0] = point[j5];
+            m4 = KhoangCach[j5];
+         }
+      }
+   }
+       for (int m1 = 0; m1 < iX; m1++)
     {
-        cin >> x >> y >> z;
-
-        point[i].setX(x);
-        point[i].setY(y);
-        point[i].setZ(z);
-    }
-    double arrx[4500] = {};
-    double arry[4500] = {};
-    double arrz[4500] = {};
-
-    freopen("slthu.txt", "r", stdin);
-    for (int i = 0; i < 4457; i++)
-        cin >> arrx[i] >> arry[i] >> arrz[i];
-
-    double minx = timMin(arrx, n), miny = timMin(arry, n);
-    double maxx = timMax(arrx, n), maxy = timMax(arry, n);
-
-    double newX[4500] = {};
-    double newY[4500] = {};
-    int iX = 0;
-    while (minx <= maxx)
-    {
-        newX[iX] = minx;
-        minx += 0.00005;
-        iX++;
-    }
-    int iY = 0;
-    while (miny <= maxy)
-    {
-        newY[iY] = miny;
-        miny += 0.00007;
-        iY++;
+       for(int m2=0;m2<4;m2++){
+          cout << _4Diem[m1][m2].getX() << " " << _4Diem[m1][m2].getY() << " " <<  _4Diem[m1][m2].getZ() << endl;
+       }
+        
     }
 
-    Obj newPoint[200];
-    int i = 0;
-    while (i < iX)
-    {
-        newPoint[i].setX(newX[i]);
-        newPoint[i].setY(newY[i]);
-        i++;
-    }
-    for (int i = 0; i < 100; i++)
-    {
-        cout << point[i].getX() << " " << point[i].getY() << " " << point[i].getZ() << endl;
-    }
-    // double KhoangCach[200][5000];
-    // KhoangCachCacDiem(point, newPoint, KhoangCach, iX);
-    // for (int i = 0; i < iX; i++)
-    // {
-    //     for (int j = 0; j < 4457; j++)
-    //     {
-    //         cout << 1 << endl;
-    //     }
-    // }
-
-    // Obj _4Diem [200][4];
-    // _4DiemGanNhat(point,KhoangCach,iX,_4Diem);
-    system("pause");
-    return 0;
+   system("pause");
+   return 0;
 }
